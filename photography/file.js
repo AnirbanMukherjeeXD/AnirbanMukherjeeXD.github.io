17c59c04c3d11c5df419268258ac1dd28982888d
Anirban Mukherjee <34798203+AnirbanMukherjeeXD@users.noreply.github.com>
2026-05-15 14:19:49 +0200

    Added photos

--- photography/file.js
+++ photography/file.js
@@ -3,7 +3,10 @@ var photos = [
     "photos/set1/DSC_0058 copy.jpg",
     "photos/set1/DSC_0063 copy.jpg",
     "photos/set1/DSC_0067 copy.jpg",
-	"photos/set1/DSC_0270 copy.jpg"
+	"photos/set1/DSC_0270 copy.jpg",
+	"photos/set1/DSC_0044 copy_BW.jpg",
+	"photos/set1/DSC_0492 copy.jpg",
+	"photos/set1/DSC_0793 copy_2.jpg"
 ];
 
 var gallery = document.getElementById("gallery");
